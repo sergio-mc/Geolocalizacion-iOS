8b03cac1022e2e2344c5f547fe3004f898c1ff67
Sergio Marrón Calle <sergiodev0106@gmail.com>
2020-01-16 20:25:56 +0100

    Initial commit

--- Maps/AppDelegate.swift
+++ Maps/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import GoogleMaps
+import GooglePlaces
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        GMSServices.provideAPIKey("AIzaSyCJ6j74ZW-RkuMrVTTQ3c5h4LSA6SCoFd8")
+        GMSPlacesClient.provideAPIKey("AIzaSyCJ6j74ZW-RkuMrVTTQ3c5h4LSA6SCoFd8")
         // Override point for customization after application launch.
         return true
     }
--- Maps/ViewController.swift
+++ Maps/ViewController.swift
@@ -1,20 +1,22 @@
-//
-//  ViewController.swift
-//  Maps
-//
-//  Created by Sergio on 16/01/2020.
-//  Copyright © 2020 sergio-mc. All rights reserved.
-//
-
 import UIKit
+import GoogleMaps
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+  // You don't need to modify the default init(nibName:bundle:) method.
 
+  override func loadView() {
+    // Create a GMSCameraPosition that tells the map to display the
+    // coordinate -33.86,151.20 at zoom level 6.
+    let camera = GMSCameraPosition.camera(withLatitude: 40.416775, longitude: -3.703790, zoom: 6.0)
+    let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+    view = mapView
 
+    // Creates a marker in the center of the map.
+    let marker = GMSMarker()
+    marker.position = CLLocationCoordinate2D(latitude: 40.416328, longitude: -3.683205)
+    marker.title = "Parque Retiro"
+    marker.snippet = "Madrid"
+    marker.map = mapView
+  }
 }
-
